4671d216185ddadc72a750db7256926deee29f03
Joe Mankin <joseaphmankin@gmail.com>
2018-10-28 10:31:37 -0500

    Rough in of all pages

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+//DATA. Holds all my survey people
+
+var friendArray = [
+    {
+        "name":"Joe",
+        "photo":"https://images.unsplash.com/photo-1506954673998-b077f05b13c7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=cbd9462960235b41245689f258d125a8&auto=format&fit=crop&w=1491&q=80",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+  ];
+
+  
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendArray;
+  
---
+++ server.js
@@ -0,0 +1,25 @@
+//DEPENDANCIES: 
+
+let express = require("express");
+let bodyParser = require("body-parser");
+
+//EXPRESS SETUP:
+
+// Tells node that we are creating an "express" server
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//ROUTER PATHS:
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//LISTENER to start server:
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
